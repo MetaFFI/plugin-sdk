5864e3e0aceda93dd82d5863cd70c0c1d1d1ea1c
tcs <tsvi.cherny-shahar@bluevoyant.com>
2021-03-21 17:05:10 +0200

    update IDL plugin interface

--- compiler/IDLPluginInterfaceHandler.go
+++ compiler/IDLPluginInterfaceHandler.go
@@ -8,7 +8,7 @@ import "C"
 var idlPluginInterfaceHandler *IDLPluginInterfaceHandler
 
 type IDLPluginInterface interface{
-	ParseIDL(idlName string, idl string) error
+	ParseIDL(idlName string, idl string) (*IDLDefinition, error)
 }
 //--------------------------------------------------------------------
 type IDLPluginInterfaceHandler struct{
@@ -21,29 +21,31 @@ func CreateIDLPluginInterfaceHandler(wrapped IDLPluginInterface){
 //--------------------------------------------------------------------
 func (this *IDLPluginInterfaceHandler) parse_idl(idl_name *C.char, idl_name_length C.uint,
 												idl *C.char, idl_length C.uint,
-												out_err **C.char, out_err_len *C.uint) {
+												out_err **C.char, out_err_len *C.uint) *C.idl_definition{
 
 	idlName := C.GoStringN(idl_name, C.int(idl_name_length))
 	idlStr := C.GoStringN(idl, C.int(idl_length))
 
-	err := this.wrapped.ParseIDL(idlName, idlStr)
+	def, err := this.wrapped.ParseIDL(idlName, idlStr)
 
 	if err != nil{
 		*out_err = C.CString(err.Error())
 		*out_err_len = C.uint(len(err.Error()))
-		return
+		return nil
 	}
+
+	return def
 }
 //--------------------------------------------------------------------
 //export parse_idl
 func parse_idl(idl_name *C.char, idl_name_length C.uint,
 	idl *C.char, idl_length C.uint,
-	out_err **C.char, out_err_len *C.uint){
+	out_err **C.char, out_err_len *C.uint) *C.idl_definition{
 
 	if idlPluginInterfaceHandler == nil{
 		panic("idlPluginInterfaceHandler is null!")
 	}
 
-	idlPluginInterfaceHandler.parse_idl(idl_name, idl_name_length, idl, idl_length, out_err, out_err_len)
+	return idlPluginInterfaceHandler.parse_idl(idl_name, idl_name_length, idl, idl_length, out_err, out_err_len)
 }
 //--------------------------------------------------------------------
