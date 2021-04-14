37629c3b671daeef91ccefa82cf3ebf042f2f4cc
tcs <tsvi.cherny-shahar@bluevoyant.com>
2021-04-14 16:47:22 +0300

    entrypoint function and openffi guest lib constants

--- compiler/go/WellKnownTags.go
+++ compiler/go/WellKnownTags.go
@@ -4,4 +4,6 @@ const(
 	TARGET_LANGUAGE="openffi_target_language"
 	FUNCTION_PATH="openffi_function_path"
 	PASS_METHOD="openffi_pass_method"
+	OPENFFI_GUEST_LIB="openffi_guest_lib"
+	ENTRYPOINT_FUNCTION="entrypoint_function"
 )
--- utils/foreign_function.h
+++ utils/foreign_function.h
@@ -5,3 +5,6 @@
 #define foreign_function_entrypoint_signature void(unsigned char*, uint64_t, unsigned char**, uint64_t*, unsigned char**, uint64_t*, uint8_t*)
 
 typedef boost::dll::detail::library_function<foreign_function_entrypoint_signature> foreign_function_entrypoint;
+
+#define function_path_entry_openffi_guest_lib "openffi_guest_lib"
+#define function_path_entry_entrypoint_function "entrypoint_function"
