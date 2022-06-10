6cb3f8a86600bfe6cc9d9ba08a24a578b0bd2994
TCs <green.fuzer@gmail.com>
2022-06-10 21:07:27 +0300

    import -> import_symbol in boost::dll library

--- utils/function_loader.hpp
+++ utils/function_loader.hpp
@@ -7,8 +7,13 @@ namespace metaffi::utils
 template<typename T>
 std::shared_ptr<typename boost::dll::detail::import_type<T>::type> load_func(const std::string& fullpath, const std::string& funcname, boost::dll::load_mode::type mode )
 {
+
 	return std::make_shared<typename boost::dll::detail::import_type<T>::type>(
-			boost::dll::import_alias<T>(fullpath, funcname, mode)
+#if BOOST_VERSION >= 107100
+		boost::dll::import_symbol<T>(fullpath, funcname, mode)
+#else
+		boost::dll::import<T>(fullpath, funcname, mode);
+#endif
 	);
 }
 //--------------------------------------------------------------------
@@ -16,7 +21,11 @@ template<typename T>
 std::shared_ptr<typename boost::dll::detail::import_type<T>::type> load_func(const boost::dll::shared_library& so, const std::string& funcname)
 {
 	return std::make_shared<typename boost::dll::detail::import_type<T>::type>(
-			boost::dll::import_alias<T>(so, funcname)
+#if BOOST_VERSION >= 107100
+		boost::dll::import_symbol<T>(so, funcname)
+#else
+		boost::dll::import<T>(so, funcname)
+#endif
 	);
 }
 //--------------------------------------------------------------------
