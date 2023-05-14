eb7cfe865b69acbaef91d718b9cc030cb801b812
TCs <green.fuzer@gmail.com>
2023-05-14 21:30:33 +0300

    (no branch): Auto stash before rebase of "main"

--- cmake/Go.cmake
+++ cmake/Go.cmake
@@ -6,43 +6,37 @@ macro(add_go_target NAME)
 endmacro()
 
 function(go_get TARGET)
-	cmake_parse_arguments("go_get"
+	cmake_parse_arguments("add_go_test"
 			"" # bool vals
 			"WORKING_DIRECTORY" # single val
 			"" # multi-vals
 			${ARGN})
 
-	if("${go_get_WORKING_DIRECTORY}" STREQUAL "")
-		set(go_get_WORKING_DIRECTORY ".")
+	if("${add_go_test_WORKING_DIRECTORY}" STREQUAL "")
+		set(add_go_test_WORKING_DIRECTORY ".")
 	endif()
-
 	add_custom_command(TARGET ${TARGET}
-			WORKING_DIRECTORY "${go_get_WORKING_DIRECTORY}"
+			WORKING_DIRECTORY .
 			COMMAND ${GOEXEC} get -v
 			COMMENT "Running \"go get\" for target ${target_name}"
 			USES_TERMINAL )
 endfunction()
 
 macro(go_build TARGET)
-	set(oneValueArgs NAME WORKING_DIRECTORY)
 	cmake_parse_arguments("go_build"
 			"" # bool vals
-			"${oneValueArgs}" # single val
+			"NAME" # single val
 			"" # multi-vals
 			${ARGN})
 
-	if("${go_build_NAME}" STREQUAL "")
+	if("${NAME}" STREQUAL "")
 		set(output_file ${TARGET})
 	else()
 		set(output_file ${go_build_NAME})
 	endif()
 
-	if("${go_build_WORKING_DIRECTORY}" STREQUAL "")
-		set(go_build_WORKING_DIRECTORY ".")
-	endif()
-
 	add_custom_command(TARGET ${TARGET}
-			WORKING_DIRECTORY ${go_build_WORKING_DIRECTORY}
+			WORKING_DIRECTORY .
 			COMMAND ${GOEXEC} build -buildmode=c-shared -gcflags=-shared -o ${PROJECT_BINARY_DIR}/${output_file}${CMAKE_SHARED_LIBRARY_SUFFIX}
 			COMMENT "Building go C-Shared dynamic library for target ${target_name} to ${PROJECT_BINARY_DIR}/${output_file}${CMAKE_SHARED_LIBRARY_SUFFIX}"
 			USES_TERMINAL )
