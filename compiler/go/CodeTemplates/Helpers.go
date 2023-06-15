4da59f3726cf3d2d9cea340fc050ea293104d60c
TCs <green.fuzer@gmail.com>
2023-06-15 09:28:14 +0300

    add text/Template

--- compiler/go/CodeTemplates/Helpers.go
+++ compiler/go/CodeTemplates/Helpers.go
@@ -2,6 +2,7 @@ package TemplateFunctions
 
 import (
 	"html/template"
+	txtTemplate "text/template"
 	"strings"
 )
 import "github.com/Masterminds/sprig/v3"
@@ -45,5 +46,45 @@ func RunTemplate(name string, templateTxt string, params interface{}, funcs ...m
 		return "", err
 	}
 	
+	return buf.String(), nil
+}
+
+
+func RunTemplateTxt(name string, templateTxt string, params interface{}, funcs ...map[string]any) (string, error) {
+
+	actualFuncs := make(map[string]any)
+
+	for _, curFuncs := range funcs {
+		for k, v := range curFuncs {
+			if _, exists := actualFuncs[k]; !exists {
+				actualFuncs[k] = v
+			}
+		}
+	}
+
+	for k, v := range templatesFuncMap {
+		if _, exists := actualFuncs[k]; !exists {
+			actualFuncs[k] = v
+		}
+	}
+
+	for k, v := range sprig.FuncMap() {
+		if _, exists := actualFuncs[k]; !exists {
+			actualFuncs[k] = v
+		}
+	}
+
+	tmp, err := txtTemplate.New(name).Funcs(actualFuncs).Parse(templateTxt)
+	if err != nil {
+		return "", err
+	}
+
+	buf := strings.Builder{}
+	err = tmp.Execute(&buf, params)
+
+	if err != nil {
+		return "", err
+	}
+
 	return buf.String(), nil
 }
