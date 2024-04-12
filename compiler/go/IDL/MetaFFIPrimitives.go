a50bb5071f9a4e39e291a565d6294afc00d72ffa
GreenFuze <6296195+GreenFuze@users.noreply.github.com>
2024-04-12 16:59:41 +0300

    .

--- compiler/go/IDL/MetaFFIPrimitives.go
+++ compiler/go/IDL/MetaFFIPrimitives.go
@@ -239,7 +239,11 @@ func (m *MetaFFITypeInfo) AsCMetaFFITypeInfo() C.struct_metaffi_type_info {
 
 	if m.Alias != "" {
 		cMetaFFIType.alias = C.CString(m.Alias)
-		cMetaFFIType.is_free_alias = C.metaffi_bool(1)
+		cMetaFFIType.is_free_alias = C.metaffi_bool(0) // TODO: change to TRUE
+		//		cMetaFFIType.is_free_alias = C.metaffi_bool(1)
+	} else {
+		cMetaFFIType.alias = nil
+		cMetaFFIType.is_free_alias = C.metaffi_bool(0)
 	}
 
 	C.set_metaffi_type_info_type(&cMetaFFIType, C.uint64_t(m.Type))
