40cb0644f6c7e316de1950e023900448ad99cdae
GreenFuze <6296195+GreenFuze@users.noreply.github.com>
2024-04-18 14:34:54 +0300

    update python3.cmake

--- cmake/Python3.cmake
+++ cmake/Python3.cmake
@@ -1,13 +1,5 @@
 include(${CMAKE_CURRENT_LIST_DIR}/Utils.cmake)
 
-# if windows
-if(WIN32)
-	set(PYEXECNAME "py")
-else()
-	set(PYEXECNAME "python3")
-endif()
-
-get_app_path(${PYEXECNAME} PYEXEC)
 
 macro(add_py_test NAME)
 	cmake_parse_arguments("add_py_test"
@@ -22,17 +14,33 @@ macro(add_py_test NAME)
 			"DEPENDENCIES" # multi-vals
 			${ARGN})
 
+	if(WIN32)
+		set(PYEXECNAME "py")
+		get_app_path(${PYEXECNAME} PYEXECFULLPATH)
+	else()
+		set(PYEXECFULLPATH "/usr/bin/python3.11")
+	endif()
+
+	message(STATUS "Python executable: ${PYEXECFULLPATH}")
+
+
 	if("${add_py_test_WORKING_DIRECTORY}" STREQUAL "")
 		set(add_go_test_WORKING_DIRECTORY .)
 	endif()
 
 	if(NOT "${add_py_test_DEPENDENCIES}" STREQUAL "")
 		foreach(DEP ${add_py_test_DEPENDENCIES})
-			execute_process(COMMAND ${PYEXEC} -m pip install ${DEP} --upgrade)
+			execute_process(COMMAND ${PYEXECFULLPATH} -m pip install ${DEP} --upgrade)
 		endforeach()
 	endif()
 
-	add_test(NAME "(python3 test) ${NAME}"
-			COMMAND ${PYEXEC} -m unittest ${NAME}
-			WORKING_DIRECTORY ${add_py_test_WORKING_DIRECTORY})
+
+
+	if(EXISTS ${PYEXECFULLPATH})
+		add_test(NAME "(python3 test) ${NAME}"
+				COMMAND ${PYEXECFULLPATH} -m unittest ${NAME}
+				WORKING_DIRECTORY ${add_py_test_WORKING_DIRECTORY})
+	else()
+		message(WARNING "Python executable not found at ${PYEXECFULLPATH}. Test ${NAME} not added.")
+	endif()
 endmacro()
